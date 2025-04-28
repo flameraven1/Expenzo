a684c7e48ddca3db9b519d51646f96aad563e90f
Hammad Hussain <hammadhussain920@gmail.com>
2025-04-28 10:43:07 +0500

    "M"

--- src/auth.ts
+++ src/auth.ts
@@ -6,8 +6,8 @@ import { User } from "./models/user";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
-    clientId: process.env.GOOGLE_CLIENT_ID!,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   })],
 
   callbacks : {
