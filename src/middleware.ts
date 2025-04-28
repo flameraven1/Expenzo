9aa2e91b2c730769ba1f2868c5c078fd86046dd3
Hammad Hussain <hammadhussain920@gmail.com>
2025-04-28 16:21:23 +0500

    "mdware"

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(req : NextRequest){
-    const getURL = new URL(req.url)
-    const urlPath = getURL.pathname
+export default async function middleware(req: NextRequest) {
+  const getURL = new URL(req.url);
+  const urlPath = getURL.pathname;
 
-    const isPublicPaths = urlPath === "/login"
-
-    const cookies = await req.cookies.get("__Host-authjs.csrf-token")
-
-    if(isPublicPaths && cookies){
-        return NextResponse.redirect(new URL("/dashboard" , req.url))
-    }
-
-    if(!isPublicPaths && !cookies){
-        return NextResponse.redirect(new URL("/login" , req.url))
-    }
+  if (urlPath.startsWith("/api/auth/callback")) {
     return NextResponse.next();
+  }
+
+  const isPublicPaths = urlPath === "/login";
+  const cookies = await req.cookies.get("authjs.session-token");
+
+  if (isPublicPaths && cookies) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  if (!isPublicPaths && !cookies) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/dashboard" , "/login" , "/dashboard/:id"],
-  }
+  matcher: ["/dashboard", "/login", "/dashboard/:id"],
+};
