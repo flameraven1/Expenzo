f590599dc1fdc91c7aee8a55fe761ef11d0b6d2a
Hammad Hussain <hammadhussain920@gmail.com>
2025-04-29 12:29:51 +0500

    "this"

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -7,7 +7,7 @@ export function SignIn() {
   return (
     <button 
       className="flex items-center justify-center bg-white border-2 border-gray-300 rounded-full w-[80%] p-4 gap-3 hover:bg-gray-100 active:scale-95 transition-all duration-300 shadow-md cursor-pointer"
-      onClick={() => signIn("google", { redirectTo: "/dashboard" })}
+      onClick={() => signIn("google")}
     >
       <div className="w-6 h-6 relative">
         <Image src="/images/google.png" alt="Google Logo" layout="fill" objectFit="contain" />
