a4959221400fa9db4ebe2a4d6eab591aac1a5f79
Hammad Hussain <hammadhussain920@gmail.com>
2025-04-29 14:24:21 +0500

    "DATABASE"

--- src/database/ConnectDB.ts
+++ src/database/ConnectDB.ts
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-export const ConnectDB = async () =>{
-    try {
-        await mongoose.connect(`${process.env.MONGO_URI}`)
-    } catch (error) {
-        console.log(error)
-    }
+export const ConnectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    console.log("Already connected to DB");
+    return;
+  }
+
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}`);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log("DB connection error:", error);
+  }
 }
